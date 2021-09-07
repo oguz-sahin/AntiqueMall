6d073925f69cfc64397d76646dfe13013c096b86
oguz sahin <oguzsahin098@gmail.com>
2021-09-07 17:33:19 +0300

    base fragment and splash add also navigation configure.

---
+++ app/src/main/java/com/example/antiquemall/ui/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.antiquemall.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.antiquemall.R
+import com.example.antiquemall.databinding.ActivityMainBinding
+import com.example.antiquemall.util.gone
+import com.example.antiquemall.util.visible
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+        setUpNavigation()
+        setNavMenuVisibility()
+    }
+
+    private fun setUpNavigation() {
+        val navController = getNavController()
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
+    private fun setNavMenuVisibility() {
+        val navController = getNavController()
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.splashFragment -> {
+                    binding.bottomNavigationView.gone()
+                }
+                else -> {
+                    binding.bottomNavigationView.visible()
+                }
+            }
+        }
+    }
+
+
+    private fun getNavController(): NavController {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragmentContainer) as NavHostFragment
+        return navHostFragment.navController
+    }
+}
---
+++ app/src/main/java/com/example/antiquemall/ui/splash/SplashFragment.kt
@@ -0,0 +1,12 @@
+package com.example.antiquemall.ui.splash
+
+import com.example.antiquemall.base.BaseFragment
+import com.example.antiquemall.databinding.FragmentSplashBinding
+
+
+class SplashFragment : BaseFragment<FragmentSplashBinding>(
+    inflate = FragmentSplashBinding::inflate
+) {
+
+
+}
